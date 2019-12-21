7af94340efa75e8d63a1ea1e783db4ec64c435eb
Harminder virk <virk.officials@gmail.com>
2019-12-21 10:18:52 +0530

    style: add docblocks

--- adonis-typings/encryption.ts
+++ adonis-typings/encryption.ts
@@ -23,11 +23,40 @@ declare module '@ioc:Adonis/Core/Encryption' {
   }
 
   export interface EncryptionContract {
+    /**
+     * Encrypt a given piece of value using the app secret. A wide range of
+     * data types are supported.
+     *
+     * - String
+     * - Arrays
+     * - Objects
+     * - Booleans
+     * - Numbers
+     * - Dates
+     *
+     * Encrypt/decrypting a date object will result in returning a date string.
+     */
     encrypt (payload: any): string,
+
+    /**
+     * Decrypt a previously encrypted value
+     */
     decrypt (payload: string): any,
+
+    /**
+     * Create a new instance of encryption with custom runtime config
+     */
     create (options?: Partial<EncryptionConfigContract>): EncryptionContract,
+
+    /**
+     * BASE64 Encode value
+     */
     base64Encode (arrayBuffer: ArrayBuffer | SharedArrayBuffer): string,
     base64Encode (data: string, encoding?: BufferEncoding): string,
+
+    /**
+     * BASE64 decode the encoded value
+     */
     base64Decode (encoded: string | Buffer, encoding?: BufferEncoding): string,
   }
 
