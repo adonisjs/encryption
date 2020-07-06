c2fbf9b5d568458e9787f419175e5eb0d8ac464d
Harminder virk <virk.officials@gmail.com>
2020-07-06 10:54:48 +0530

    chore: add prettier

--- providers/EncryptionProvider.ts
+++ providers/EncryptionProvider.ts
@@ -5,7 +5,7 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
-*/
+ */
 
 import { IocContract } from '@adonisjs/fold'
 import { Encryption } from '../src/Encryption'
@@ -14,12 +14,12 @@ import { Encryption } from '../src/Encryption'
  * Encryption provider to binding encryption class to the container
  */
 export default class EncryptionProvider {
-  constructor (protected $container: IocContract) {}
+	constructor(protected $container: IocContract) {}
 
-  public register () {
-    this.$container.singleton('Adonis/Core/Encryption', () => {
-      const Config = this.$container.use('Adonis/Core/Config')
-      return new Encryption({ secret: Config.get('app.appKey') })
-    })
-  }
+	public register() {
+		this.$container.singleton('Adonis/Core/Encryption', () => {
+			const Config = this.$container.use('Adonis/Core/Config')
+			return new Encryption({ secret: Config.get('app.appKey') })
+		})
+	}
 }
